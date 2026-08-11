149002e450d515972474baf0be1c4648316827e3
Aabhasmishra <104935350+Aabhasmishra@users.noreply.github.com>
2026-08-11 10:20:41 +0530

    Update mainServer.js

--- Backend/mainServer.js
+++ Backend/mainServer.js
@@ -23,7 +23,7 @@ initializeApp({
   credential: cert({
     projectId:   process.env.FIREBASE_PROJECT_ID,
     clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-    privateKey:  process.env.FIREBASE_PRIVATE_KEY.replace(/^"|"$/g, '').trim(),
+    privateKey:  require('fs').readFileSync('./firebase-private-key.pem', 'utf8'),
   }),
 });
 
